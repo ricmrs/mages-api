34866cbacd38b62c825e53b5c8bfccb79e6f5e0e
Ricardo <83619661+ricmrs@users.noreply.github.com>
2024-10-22 20:06:21 -0300

    Data images path change from png to webp

--- assets/data/books.js
+++ assets/data/books.js
@@ -3,28 +3,28 @@ export default [
       "title": "The Arcane Grimoire",
       "releaseDate": "1998-05-14",
       "description": "Um antigo tomo que contém segredos e feitiços poderosos, passados de geração em geração entre os maiores magos. Este livro é considerado uma das maiores fontes de conhecimento sobre magia arcana.",
-      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/the-arcane-grimoire.png",
+      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/the-arcane-grimoire.webp",
       "index": 0,
   },
   {
       "title": "Elemental Mastery: The Four Pillars of Magic",
       "releaseDate": "2005-11-02",
       "description": "Um guia abrangente sobre o domínio dos quatro elementos principais: Fogo, Terra, Água e Ar. O livro oferece instruções detalhadas sobre como manipular cada elemento e suas respectivas magias.",
-      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/elemental-mastery.png",
+      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/elemental-mastery.webp",
       "index": 1,
   },
   {
       "title": "Mysteries of the Mystic Arts",
       "releaseDate": "2012-07-21",
       "description": "Explora as artes místicas, incluindo rituais, encantamentos e a filosofia por trás da magia. Ideal para magos que desejam aprofundar seus conhecimentos sobre a natureza esotérica da magia.",
-      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/mysteries-of-the-mystic-arts.png",
+      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/mysteries-of-the-mystic-arts.webp",
       "index": 2,
   },
   {
       "title": "The Enchanter's Compendium",
       "releaseDate": "2017-09-18",
       "description": "Uma coletânea de encantamentos e feitiços práticos para magos de todos os níveis. Inclui desde feitiços básicos até conjurações complexas, além de dicas sobre como personalizar sua própria magia.",
-      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/the-enchanter.png",
+      "image": "https://raw.githubusercontent.com/ricmrs/mages-api/main/public/images/books/the-enchanter.webp",
       "index": 3,
   }
 ]
